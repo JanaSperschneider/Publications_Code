df4aed33e561505bf6312f7c388ef5c933b9bf3f
Jana Sperschneider <jana.sperschneider@csiro.au>
2020-07-10 07:05:31 +1000

    Update Figure10.R

--- 2019_12_StemRust_smRNA_Paper/Figure10/Figure10.R
+++ 2019_12_StemRust_smRNA_Paper/Figure10/Figure10.R
@@ -1,27 +1,4 @@
 #################################################################################################################################################################
-theme_Publication <- function(base_size=12, base_family="helvetica") {
-  library(grid)
-  library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(face = "bold",
-                                      size = rel(1.5), hjust = 0.5),
-            text = element_text(),
-            panel.background = element_rect(colour = NA),
-            plot.background = element_rect(colour = NA),
-            panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold",size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(face = "bold",size = rel(1)), 
-            axis.line = element_line(colour="black"),
-            axis.ticks = element_line(),
-            plot.margin=unit(c(10,5,5,5),"mm"),
-            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-            strip.text = element_text(face="bold")
-    ))
-  
-}
-
 scale_fill_Publication <- function(...){
   library(scales)
   discrete_scale("fill","Publication",manual_pal(values = c("#dfc27d","#018571","#dfc27d","#018571",
@@ -49,7 +26,6 @@ scale_colour_Publication <- function(...){
 #################################################################################################################################################################
 library(ggplot2)
 library(reshape2)
-setwd("H:/ANU/InProgress/Project_Pgt_smRNA_V2/Figures_Revision/Figure10")
 #################################################################################################################################################################
 # Record the % of methylated TEs that are sRNA_plus
 df <- data.frame(methylation <- c("Late infection", "Germinated spores"),
